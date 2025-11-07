d2086be19dcc8519baed40bb2cb990e70bfc186d
平泉 遥輝 <ha-hiraizumi@kentem.co.jp>
2025-11-07 07:11:29 +0000

    微修正

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/simple-react-app/", // リポジトリ名
+  base: "/my-todo/", // リポジトリ名
   test: {
     globals: true,
     includeSource: ["src/**/*.{ts,tsx}"],
